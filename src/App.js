7cb472bdab30192e44d42cc3af41af3e33875146
Miguel <miguezaga2@hotmail.com>
2022-11-11 09:26:54 -0500

    add media queries

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ function App() {
         <h1 className="main__title">
           We are launching <b className="bold">soon!</b>
         </h1>
-        <p className="main__subtitle">
-          Subscribe and get notified Notify Me &copy;
-        </p>
+        <p className="main__subtitle">Subscribe and get notified</p>
         <form noValidate className="main__form form">
           <input
             className="form__input"
